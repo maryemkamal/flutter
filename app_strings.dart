516ffd00d8c2db4ff69c70063ed9d1ede5b3e81e
maryemkamal <121868686+maryemkamal@users.noreply.github.com>
2025-07-21 03:15:58 +0300

    Add files via upload

---
+++ app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static String loginScreenWelcomeText = 'اهلا بك نحت سعداء بعودتك ';
+  static String loginScreenPhonenumberTextFiled = ' ادخل رقم الهاتف ';
+  static String loginScreenPasswordTextFiled = 'ادخل كلمه المرور  ';
+  static String loginScreenForgetPass = 'اهلا بك نحت سعداء بعودتك ';
+}
--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Login.dart';
+import 'package:iti_project/login_screen.dart';
+import 'package:iti_project/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(title: 'Flutter Home Page'),
     );
   }
 }
